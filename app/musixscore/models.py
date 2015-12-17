a844edbf92f611c2c491cadf219aae5dd5eca488
Lemuel Jay Vallinas <lemueljayvallinas@gmail.com>
2015-12-17 19:36:49 +0800

    Create models

--- app/musixscore/models.py
+++ app/musixscore/models.py
@@ -8,5 +8,20 @@ class Genre(models.Model):
     genre = models.CharField(max_length=200)
 
 
+class Artist(models.Model):
+    artist = models.CharField(max_length=200)
 
-class
+
+class Album(models.Model):
+    album = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist)
+
+
+class Song(models.Model):
+    song = models.CharField(max_length=200)
+    cd = models.ForeignKey(Album)
+
+
+class Work(models.Model):
+    work = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist)
